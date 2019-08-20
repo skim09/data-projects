7bce30bd876b40e91938d2f01eb7c24490ef2b4d
Sangwon Kim <sangwonkim@Sangwons-MacBook-Pro.local>
2019-08-20 18:41:25 +0900

    Added all supporting files and data

---
+++ manchester-united-network-analysis/newcastle_data/newcastle_script.R
@@ -0,0 +1,78 @@
+## Newcastle United match script
+## Loading necessary packages
+library(igraph)
+library(dplyr)
+
+## Setting working directory, importing data files
+setwd("/Users/swkim728/Desktop/wesleyan/fall_2018/qac241/final_project/newcastle_data")
+first_half = read.csv("first_half_data.csv", header = T) %>% filter(!to %in% c(0,98,99))
+second_half = read.csv("second_half_data.csv", header = T) %>% filter(!to %in% c(0,98,99))
+full_game = first_half %>% rbind(second_half)
+
+##creating adjacency matrix
+adj_matrix1 = first_half %>% 
+  graph_from_data_frame(directed = T) %>%
+  get.adjacency() %>%
+  as.matrix()
+g1 = graph_from_adjacency_matrix(adj_matrix1, mode = "directed", weighted = T)
+for (i in 1:nrow(adj_matrix1)) {
+  for (j in 1:ncol(adj_matrix1)) {
+    max_value = max(adj_matrix1[i,])
+    min_value = sort(adj_matrix1, decreasing = T)[10]
+    if (adj_matrix1[i,j] < min_value) {
+      adj_matrix1[i,j] = 0
+    }
+  }
+}
+vg1 = graph_from_adjacency_matrix(adj_matrix1, mode = "directed", weighted = T)
+##write.graph(graph=g, file="firsthalf.graphml", format="graphml")
+adj_matrix1A = first_half %>% 
+  graph_from_data_frame(directed = F) %>%
+  get.adjacency() %>%
+  as.matrix()
+g1A = graph_from_adjacency_matrix(adj_matrix1A, mode = "undirected", weighted = T)
+##write.graph(graph=g1A, file="firsthalfA.graphml", format="graphml")
+
+##for second half
+adj_matrix2 = second_half %>% 
+  graph_from_data_frame(directed = T) %>%
+  get.adjacency() %>%
+  as.matrix()
+adj_matrix2["10",] = adj_matrix2["10",] + adj_matrix2["7",]
+adj_matrix2[,"10"] = adj_matrix2[,"10"] + adj_matrix2[,"7"]
+adj_matrix2 = adj_matrix2[-12,-12]
+g2 = graph_from_adjacency_matrix(adj_matrix2, mode = "directed", weighted = T)
+for (i in 1:nrow(adj_matrix2)) {
+  for (j in 1:ncol(adj_matrix2)) {
+    max_value = max(adj_matrix2[i,])
+    min_value = sort(adj_matrix2, decreasing = T)[10]
+    if (adj_matrix2[i,j] < min_value) {
+      adj_matrix2[i,j] = 0
+    }
+  }
+}
+vg2 = graph_from_adjacency_matrix(adj_matrix2, mode = "directed", weighted = T)
+##write.graph(graph=vg2, file="secondhalfcombined.graphml", format="graphml")
+
+adj_matrix2a = second_half %>% 
+  graph_from_data_frame(directed = F) %>%
+  get.adjacency() %>%
+  as.matrix()
+adj_matrix2a["10",] = adj_matrix2["10",] + adj_matrix2["7",]
+adj_matrix2a[,"10"] = adj_matrix2[,"10"] + adj_matrix2[,"7"]
+adj_matrix2a = adj_matrix2a[-12,-12]
+g2a = graph_from_adjacency_matrix(adj_matrix2a, mode = "undirected", weighted = T)
+##write.graph(graph=g2a, file="secondhalfcombineda.graphml", format="graphml")
+
+adj_matrixf = first_half %>% 
+  graph_from_data_frame(directed = T) %>%
+  get.adjacency() %>%
+  as.matrix()
+gf = graph_from_adjacency_matrix(adj_matrixf, mode = "directed", weighted = T)
+
+firsthalf_eigen = eigen_centrality(g1, directed = T)
+sort(firsthalf_eigen$vector, decreasing = T)
+secondhalf_eigen = eigen_centrality(g2, directed = T)
+sort(secondhalf_eigen$vector, decreasing = T)
+fullgame_eigen = eigen_centrality(gf, directed = T)
+sort(fullgame_eigen$vector, decreasing = T)
